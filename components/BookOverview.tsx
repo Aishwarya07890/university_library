2bcca3a3a37bc09729ceca70a19f80d7c6f5da4d
Aishwarya07890 <141170274+Aishwarya07890@users.noreply.github.com>
2025-01-15 10:46:36 +0530

    implement borrow records and finish the app

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Image from "next/image";
 import BookCover from "@/components/BookCover";
 import BorrowBook from "@/components/BorrowBook";
-// import { db } from "@/database/drizzle";
-// import { users } from "@/database/schema";
-// import { eq } from "drizzle-orm";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
-// interface Props extends Book {
-//   userId: string;
-// }
-const BookOverview = ({
+interface Props extends Book {
+  userId: string;
+}
+const BookOverview = async ({
   title,
   author,
   genre,
--- components/BorrowBook.tsx
+++ components/BorrowBook.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { toast } from "@/hooks/use-toast";
-//import { borrowBook } from "@/lib/actions/book";
+import { borrowBook } from "@/lib/actions/book";
 
 interface Props {
   userId: string;
